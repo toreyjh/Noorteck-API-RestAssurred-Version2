d4142e5f98c8c73c4ac0811d446e988227577ea6
Torey Hartney <tjhartney@gmail.com>
2023-01-22 11:26:49 -0500

    committing

--- src/test/java/com/noorteck/qa/runners/Runner3.java
+++ src/test/java/com/noorteck/qa/runners/Runner3.java
@@ -7,13 +7,13 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-					features = "src/test/resources/hr/US100-CreateNewEmployee.feature",
+					features = "src/test/resources/hr/",
 					glue = "com.noorteck.qa.steps",					
 							plugin = {"summary","pretty", "html:target/cucumber-reports.html",
 									"json:target/cucumber-reports.json",					
 									"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 					monochrome = true
 						)
-public class Runner {
+public class Runner3 {
 
 }
